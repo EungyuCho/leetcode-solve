865a2c6322f1d35c24b059438ef733bf841b0176
Jay <53712717+EungyuCho@users.noreply.github.com>
2021-07-04 21:32:11 +0900

    Time: 184 ms (65.22%), Space: 45 MB (60.87%) - LeetHub

---
+++ perfect-squares/perfect-squares.ts
@@ -0,0 +1,13 @@
+function numSquares(n: number): number {
+  const dp = [0];
+  
+  for (let i = 1; i <= n; i++) {
+    dp[i] = Infinity;
+      
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j]+1);
+    } 
+  }
+    
+  return dp[n];
+};
